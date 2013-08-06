6677de7351f5bb32d6cd463d1aa1e9b212d3024c
StefChri <stefaan.ch@gmail.com>
2013-08-06 22:30:51 +0200

    Started processing answer on heat map

--- public/_scripts/js/answers.js
+++ public/_scripts/js/answers.js
@@ -6,6 +6,12 @@
  */
 var _active = 1;
 var _max = 12;
+var alertM = false;
+var _answers = {};
+var _data = {};
+var _hmdata = [];
+var _wijken = {};
+var _map;
 
 //NAVIGATION
 function start(){
@@ -14,6 +20,17 @@ function start(){
             $("#test").show();
         });
     });
+    
+    $(document).keydown(function(e){
+        if (e.which == 37) { 
+           previous();
+           return false;
+        }
+        if (e.which == 39) { 
+           next();
+           return false;
+        }
+});
 }
 
 function showAnswer(id, dir)
@@ -34,7 +51,7 @@ function next()
             showAnswer(++_active, true);
             if (_active === _max)
                $(".next a").html("Finish"); 
-           else
+            else
             {
                 $(".next a").html("Next &rarr;");
                 $(".previous").removeClass("disabled"); 
@@ -75,7 +92,6 @@ function checkIfFilledIn()
 {
     var flag = false;
     $(".question[data-question='" + _active + "'] input[type='radio']").each(function(i,v){
-        console.log("checking");
         if ($(v).is(':checked')) {
             flag = true;
         }
@@ -85,24 +101,163 @@ function checkIfFilledIn()
 
 function alertMsg()
 {
-    var msg =   '<div class="alert fade in span12">'+
+    if (!alertM) {
+        var msg =   '<div class="alert fade in span12">'+
                     '<button type="button" class="close" data-dismiss="alert">&times;</button>'+
-                    '<strong>Hey there!</strong> You can only switch questions if you filled this one in.'+
+                    '<strong>Hey there!</strong> You can only switch questions if you answered this one.'+
                 '</div>';
-    $("#test").append(msg);
-    $(".alert").alert();
-    setTimeout(function () {
-        $(".alert").alert('close')
-    }, 3000);
+        $("#test").append(msg);
+        $(".alert").alert();
+        setTimeout(function () {
+            $(".alert").alert('close');
+            alertM = false;
+        }, 3000);
+        alertM = true;
+    }
+    
 }
 
 function finish()
 {
+    $(".question").each(function(index,value){
+        var number = $(value).data("question");
+        _answers[number] = $(value).find("input[type='radio']:checked").first().val();
+    });
     $("#test").hide();
+    
+    $("#map_span").show();
+    initializeMap();
+    analyseAnswers();
+}
+
+function analyseAnswers()
+{
+    $.each(_answers, function(ind, va){
+        switch(ind)
+        {
+            //AGE
+            case "1":   //DOES NOTHING YET
+                        break;
+                     
+            //PARKS
+            case "2":   
+                    if(va != "2")
+                    {
+                        var set = _data["Parken"].value.Parken;
+                        $.each(set, function(i,v){
+                            getCenterPoint(v.coords, 1);
+                        }); 
+                    }
+                    break;
+           
+            //KIDS
+            case "3":   console.log(3);
+                        break;
+                        
+            //COMFORT
+            case "4":   console.log(4);
+                        break;
+            case "5":   console.log(5);
+                        break;
+            case "6":   console.log(6);
+                        break;
+            case "7":   console.log(7);
+                        break;
+            case "8":   console.log(8);
+                        break;
+            case "9":   console.log(9);
+                        break;
+            case "10":   console.log(10);
+                        break;
+            case "11":   console.log(11);
+                        break;
+            case "12":   console.log(12);
+                        break;
+            
+                
+        }
+    });
+    placeMap();
+}
+
+function loadDataSets()
+{
+    $.getJSON(_baseUrl + "api/dataset", {}, function(data, status, jqXHR){
+        $.each(data.datasets, function(i,v){
+            for (key in v.value)
+            {
+                _data[key] = v;
+            }
+            
+        });
+        console.log(_data);
+        $.each(data.datasets[0].value.Wijken, function(i,v){
+           _wijken[v.wijk] = 0;
+        });
+    });
+    
+}
+
+function getCenterPoint(coords, impact)
+{
+    //SPLIT COORDINATES FOR SEARCHING
+    var coordsArray = coords.split(' ');
+    var coLL = [];
+    $.each(coordsArray, function(i,v)
+    {
+        var co = {};
+        co["lat"] = v.split(",")[1];
+        co["lng"] = v.split(",")[0];
+        coLL.push(co);
+    });
+    
+    //SEARCH FOR MIN AND MAX VALUES
+    var xMin = coLL[0].lat;
+    var xMax = coLL[0].lat;
+    var yMin = coLL[0].lng;
+    var yMax = coLL[0].lng;
+    $.each(coLL, function(i,v)
+    {
+        if (xMin > v.lat) {
+            xMin = v.lat;
+        }
+        if (xMax < v.lat) {
+            xMin = v.lat;
+        }
+        if (yMin > v.lng) {
+            yMin = v.lng;
+        }
+        if (yMax < v.lng) {
+            yMin = v.lng;
+        }
+    });
+    hmpoint = {};
+    hmpoint["lat"] = xMin + ((xMax - xMin) / 2);
+    hmpoint["lng"] = yMin + ((yMax - yMin) / 2);
+    hmpoint["count"] = parseInt(impact);
+    _hmdata.push(hmpoint);
+}
+
+function placeMap()
+{
+    var heatmap = new HeatmapOverlay(_map, {
+        "radius":20,
+        "visible":true, 
+        "opacity":60
+    });
+    var testData={
+            max: 99999,
+            data: _hmdata
+    };
+    console.log(testData);
+    heatmap.setDataSet(testData);
 }
 
 
-$(document).ready(function(){
+$(document).ready(function()
+{
+    
+    
     $(".question").each(function(ind,val){
         if ($(val).data('question') === 1) 
             $(val).show();
@@ -110,4 +265,16 @@ $(document).ready(function(){
             $(val).hide();
     });
     $(".previous").addClass("disabled");
+    
+    loadDataSets();
 });
+
+function initializeMap()
+{
+    var mapOptions = {
+          center: new google.maps.LatLng(51.0625664653974, 3.76548313370094),
+          zoom: 11,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+    _map = new google.maps.Map(document.getElementById("gmap"),mapOptions);
+}
